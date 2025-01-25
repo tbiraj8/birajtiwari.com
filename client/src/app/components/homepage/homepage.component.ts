0ef072e5234c066ac0e110697612896fba61c8dc
Biraj Tiwari <tiwaribiraj8@gmail.com>
2025-01-25 17:20:01 -0500

    Added Change in ui

--- client/src/app/components/homepage/homepage.component.ts
+++ client/src/app/components/homepage/homepage.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Typewriter from 'typewriter-effect/dist/core'; // Import the library
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css']
 })
-export class HomepageComponent {}
+export class HomepageComponent implements OnInit {
+  ngOnInit(): void {
+    const typewriter = new Typewriter('#typewriter', {
+      loop: true,
+      delay: 75,
+    });
+
+    typewriter
+      .typeString('Innovative Software Engineer')
+      .pauseFor(1000)
+      .deleteAll()
+      .typeString('Cloud Enthusiast')
+      .pauseFor(1000)
+      .deleteAll()
+      .typeString('Problem Solver')
+      .pauseFor(1000)
+      .start();
+  }
+}
