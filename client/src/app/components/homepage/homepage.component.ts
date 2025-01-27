6099a27d26db91cd52544b4c175aac102feef5a5
Biraj Tiwari <tiwaribiraj8@gmail.com>
2025-01-27 11:13:34 -0500

    added dynamodb

--- client/src/app/components/homepage/homepage.component.ts
+++ client/src/app/components/homepage/homepage.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import Typewriter from 'typewriter-effect/dist/core'; // Import the library
 import { ApiService } from '../../../service/api.service';
-
+import { JsonPipe } from '@angular/common'; // Import JsonPipe
 @Component({
   selector: 'app-homepage',
   standalone: true,
