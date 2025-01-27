58c7e6551ce27677e04ef54edbde2c766bfbf240
Biraj Tiwari <tiwaribiraj8@gmail.com>
2025-01-27 13:15:54 -0500

    removed lambda from repo and iaac

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,23 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http'; 
-import { FormsModule } from '@angular/forms'; 
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { routes } from './app.routes'; 
+import { routes } from './app.routes';
 
 @NgModule({
-  declarations: [
-    AppComponent, 
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule, 
-    FormsModule, 
-    RouterModule.forRoot(routes, { useHash: true }) 
-  ],
-  providers: [
-  ],
-  bootstrap: [AppComponent] 
+ declarations: [
+   AppComponent,
+ ],
+ imports: [
+   BrowserModule,
+   HttpClientModule,
+   FormsModule,
+   RouterModule.forRoot(routes, { useHash: false })
+ ],
+ providers: [],
+ bootstrap: [AppComponent]
 })
 export class AppModule {}
